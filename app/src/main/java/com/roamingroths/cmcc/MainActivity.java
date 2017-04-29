4367bc7acd53127b2789e6665541cdabcc11fd4d
Parker Roth <parkeroth@gmail.com>
2017-04-29 17:32:45 +0200

    Basic auth for MainActivity

--- app/src/main/java/com/roamingroths/cmcc/MainActivity.java
+++ app/src/main/java/com/roamingroths/cmcc/MainActivity.java
@@ -3,6 +3,7 @@ package com.roamingroths.cmcc;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
@@ -10,17 +11,34 @@ import android.support.v7.widget.RecyclerView;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
+
+import com.firebase.ui.auth.AuthUI;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity implements
     ObservationAdapter.ObservationAdapterOnClickHandler {
 
+  public static final int RC_SIGN_IN = 1;
+
+  private static final String ANONYMOUS = "anonymous";
+
   private RecyclerView mRecyclerView;
   private ObservationAdapter mObservationAdapter;
+  private String mUsername = ANONYMOUS;
+
+  // Firebase stuff
+  private FirebaseAuth mFirebaseAuth;
+  private FirebaseAuth.AuthStateListener mAuthStateListener;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+
+    // Init Firebase stuff
+    mFirebaseAuth = FirebaseAuth.getInstance();
 
     mObservationAdapter = new ObservationAdapter(this, this);
 
@@ -40,6 +58,47 @@ public class MainActivity extends AppCompatActivity implements
         mObservationAdapter.notifyItemInserted(mObservationAdapter.addEntry());
       }
     });
+
+    mAuthStateListener = new FirebaseAuth.AuthStateListener() {
+      @Override
+      public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+        FirebaseUser user = firebaseAuth.getCurrentUser();
+        if (user != null) {
+          // user signed in
+          onSignedInInit(user);
+        } else {
+          // user signed out
+          onSignedOutCleanup();
+          startActivityForResult(
+              AuthUI.getInstance().createSignInIntentBuilder()
+                  .setProviders(AuthUI.GOOGLE_PROVIDER)
+                  .setIsSmartLockEnabled(false)
+                  .build(),
+              RC_SIGN_IN);
+        }
+      }
+    };
+  }
+
+  private void onSignedInInit(FirebaseUser user) {
+    mUsername = user.getDisplayName();
+    Toast.makeText(this, "Username: " + mUsername, Toast.LENGTH_LONG).show();
+  }
+
+  private void onSignedOutCleanup() {
+
+  }
+
+  @Override
+  protected void onResume() {
+    super.onResume();
+    mFirebaseAuth.addAuthStateListener(mAuthStateListener);
+  }
+
+  @Override
+  protected void onPause() {
+    super.onPause();
+    mFirebaseAuth.removeAuthStateListener(mAuthStateListener);
   }
 
   @Override
