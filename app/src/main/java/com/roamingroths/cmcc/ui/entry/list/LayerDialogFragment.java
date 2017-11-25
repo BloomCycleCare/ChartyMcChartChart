157c40cff14c88f0bdfe0d5a8bcd4cb1b4b676ab
Parker Roth <parkeroth@gmail.com>
2017-11-25 11:41:53 +0100

    ViewPager for overlay dialog

--- app/src/main/java/com/roamingroths/cmcc/ui/entry/list/LayerDialogFragment.java
+++ app/src/main/java/com/roamingroths/cmcc/ui/entry/list/LayerDialogFragment.java
@@ -2,26 +2,20 @@ package com.roamingroths.cmcc.ui.entry.list;
 
 import android.app.Dialog;
 import android.content.Context;
-import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.DialogFragment;
-import android.support.v7.app.AlertDialog;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.Button;
 
 import com.roamingroths.cmcc.R;
-
-import java.util.HashMap;
-import java.util.LinkedHashMap;
-import java.util.Map;
-
-import io.reactivex.functions.Consumer;
-import io.reactivex.subjects.PublishSubject;
 
 /**
  * Created by parkeroth on 11/19/17.
@@ -29,8 +23,9 @@ import io.reactivex.subjects.PublishSubject;
 
 public class LayerDialogFragment extends DialogFragment {
 
-  private LayerAdapter mAdapter;
-  private RecyclerView mRecyclerView;
+  private Button mButtonCancel;
+  private Button mButtonClear;
+  private ViewPager mViewPager;
   private EntryListView mEntryListView;
 
   @Override
@@ -42,116 +37,63 @@ public class LayerDialogFragment extends DialogFragment {
   @NonNull
   @Override
   public Dialog onCreateDialog(Bundle savedInstanceState) {
-    LayoutInflater inflater = getActivity().getLayoutInflater();
+    Dialog dialog = super.onCreateDialog(savedInstanceState);
+    return dialog;
+  }
 
-    View view = inflater.inflate(R.layout.dialog_layer, null, false);
-    mRecyclerView = (RecyclerView) view.findViewById(R.id.layer_recycler_view);
+  @Nullable
+  @Override
+  public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+    View view = inflater.inflate(R.layout.dialog_layer, container);
 
-    String[] values = getActivity().getResources().getStringArray(R.array.pref_wellness_option_values);
-    String[] keys = getActivity().getResources().getStringArray(R.array.pref_wellness_option_keys);
-    Map<String, String> wellnessItems = new HashMap<>();
-    for (int i=0; i < keys.length; i++) {
-      wellnessItems.put(keys[i], values[i]);
-    }
-    mAdapter = new LayerAdapter(wellnessItems, getActivity());
-    mAdapter.clickStream().subscribe(new Consumer<String>() {
+    mButtonCancel = view.findViewById(R.id.button_cancel);
+    mButtonCancel.setOnClickListener(new View.OnClickListener() {
       @Override
-      public void accept(String s) throws Exception {
-        mEntryListView.setOverlay(s);
+      public void onClick(View view) {
         dismiss();
       }
     });
-
-    mRecyclerView.setLayoutManager(
-        new LinearLayoutManager(getActivity(), LinearLayoutManager.VERTICAL, false));
-    mRecyclerView.setAdapter(mAdapter);
-
-    AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-    builder.setView(view);
-    builder.setTitle("Select an item");
-    builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+    mButtonClear = view.findViewById(R.id.button_clear);
+    mButtonClear.setOnClickListener(new View.OnClickListener() {
       @Override
-      public void onClick(DialogInterface dialogInterface, int i) {
-        dialogInterface.dismiss();
+      public void onClick(View view) {
+        mEntryListView.setOverlay("");
+        dismiss();
       }
     });
-    builder.setPositiveButton("Clear", new DialogInterface.OnClickListener() {
-      @Override
-      public void onClick(DialogInterface dialogInterface, int i) {
-        mEntryListView.clearOverlay();
-        dialogInterface.dismiss();
-      }
-    });
+    mViewPager = view.findViewById(R.id.layer_view_pager);
+    mViewPager.setAdapter(new LayerPagerAdapter(getChildFragmentManager()));
 
-    return builder.create();
+    return view;
   }
 
-  private static class LayerAdapter extends RecyclerView.Adapter<LayerOptionViewHolder> {
-    private final Map<Integer, String> wellnessItemIndex = new HashMap<>();
-    private final Map<String, String> wellnessItems = new LinkedHashMap<>();
-    private final Context mContex;
-    private final PublishSubject<String> mClickPublisher = PublishSubject.create();
+  private static class LayerPagerAdapter extends FragmentStatePagerAdapter {
 
-    public LayerAdapter(Map<String, String> wellnessItems, Context context) {
-      mContex = context;
-      int index = 0;
-      for (Map.Entry<String, String> entry : wellnessItems.entrySet()) {
-        this.wellnessItemIndex.put(index++, entry.getKey());
-        this.wellnessItems.put(entry.getKey(), entry.getValue());
+    public LayerPagerAdapter(FragmentManager fm) {
+      super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+      int keyId = position == 0 ? R.array.pref_wellness_option_keys : R.array.pref_symptom_option_keys;
+      int valId = position == 0 ? R.array.pref_wellness_option_values : R.array.pref_symptom_option_values;
+      return LayerListFragment.newInstance(keyId, valId);
+    }
+
+    @Override
+    public int getCount() {
+      return 2;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+      switch (position) {
+        case 0:
+          return "Wellness";
+        case 1:
+          return "Symptom";
       }
-    }
-
-    public PublishSubject<String> clickStream() {
-      return mClickPublisher;
-    }
-
-    @Override
-    public LayerOptionViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-      int layoutIdForListItem = R.layout.layer_list_item;
-      LayoutInflater inflater = LayoutInflater.from(mContex);
-      boolean shouldAttachToParentImmediately = false;
-      View view = inflater.inflate(layoutIdForListItem, parent, shouldAttachToParentImmediately);
-
-      return new LayerOptionViewHolder(view, mClickPublisher);
-    }
-
-    @Override
-    public void onBindViewHolder(LayerOptionViewHolder holder, int position) {
-      String key = wellnessItemIndex.get(position);
-      holder.bind(key, wellnessItems.get(key));
-    }
-
-    @Override
-    public int getItemCount() {
-      return wellnessItems.size();
-    }
-  }
-
-  private static class LayerOptionViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
-    private TextView mOptionTitle;
-    private PublishSubject<String> mPublisher;
-
-    private String key;
-
-    public LayerOptionViewHolder(View itemView, PublishSubject<String> publisher) {
-      super(itemView);
-      itemView.setOnClickListener(this);
-      mPublisher = publisher;
-      mOptionTitle = (TextView) itemView.findViewById(R.id.tv_layer_item);
-    }
-
-    public void bind(String key, String value) {
-      this.key = key;
-      mOptionTitle.setText(value);
-    }
-
-    public String getKey() {
-      return key;
-    }
-
-    @Override
-    public void onClick(View view) {
-      mPublisher.onNext(key);
+      return null;
     }
   }
 }
---
+++ app/src/main/java/com/roamingroths/cmcc/ui/entry/list/LayerListFragment.java
@@ -0,0 +1,167 @@
+package com.roamingroths.cmcc.ui.entry.list;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.DialogFragment;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.roamingroths.cmcc.R;
+
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import io.reactivex.functions.Consumer;
+import io.reactivex.subjects.PublishSubject;
+
+/**
+ * Created by parkeroth on 11/23/17.
+ */
+
+public class LayerListFragment extends Fragment {
+
+  public static final String KEYS_ARG = "keys";
+  public static final String VALS_ARG = "vals";
+
+  private int mKeyArrayId;
+  private int mValArrayId;
+
+  private LayerAdapter mAdapter;
+  private RecyclerView mRecyclerView;
+  private EntryListView mEntryListView;
+  private DialogFragment mParent;
+
+  static LayerListFragment newInstance(int keyArrayId, int valArrayId) {
+    LayerListFragment fragment = new LayerListFragment();
+    Bundle args = new Bundle();
+    args.putInt(KEYS_ARG, keyArrayId);
+    args.putInt(VALS_ARG, valArrayId);
+    fragment.setArguments(args);
+    return fragment;
+  }
+
+  @Override
+  public void onAttach(Context context) {
+    super.onAttach(context);
+    mEntryListView = (EntryListView) getActivity();
+  }
+
+  @Override
+  public void onCreate(@Nullable Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+
+    Bundle args = getArguments();
+    mKeyArrayId = args.getInt(KEYS_ARG);
+    mValArrayId = args.getInt(VALS_ARG);
+  }
+
+  @Nullable
+  @Override
+  public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+    View view = inflater.inflate(R.layout.fragment_layer_list, null, false);
+    mRecyclerView = view.findViewById(R.id.layer_recycler_view);
+
+    mParent = (DialogFragment) getParentFragment();
+
+    String[] keys = getResources().getStringArray(mKeyArrayId);
+    String[] vals = getResources().getStringArray(mValArrayId);
+    Map<String, String> wellnessItems = new HashMap<>();
+    for (int i=0; i < keys.length; i++) {
+      wellnessItems.put(keys[i], vals[i]);
+    }
+    mAdapter = new LayerAdapter(wellnessItems, getActivity());
+    mAdapter.clickStream().subscribe(new Consumer<String>() {
+      @Override
+      public void accept(String s) throws Exception {
+        mEntryListView.setOverlay(s);
+        mParent.dismiss();
+      }
+    });
+
+    mRecyclerView.setLayoutManager(
+        new LinearLayoutManager(getActivity(), LinearLayoutManager.VERTICAL, false));
+    mRecyclerView.setAdapter(mAdapter);
+
+    return view;
+  }
+
+  private static class LayerAdapter extends RecyclerView.Adapter<LayerOptionViewHolder> {
+    private final Map<Integer, String> wellnessItemIndex = new HashMap<>();
+    private final Map<String, String> wellnessItems = new LinkedHashMap<>();
+    private final Context mContex;
+    private final PublishSubject<String> mClickPublisher = PublishSubject.create();
+
+    public LayerAdapter(Map<String, String> wellnessItems, Context context) {
+      mContex = context;
+      int index = 0;
+      for (Map.Entry<String, String> entry : wellnessItems.entrySet()) {
+        this.wellnessItemIndex.put(index++, entry.getKey());
+        this.wellnessItems.put(entry.getKey(), entry.getValue());
+      }
+    }
+
+    public PublishSubject<String> clickStream() {
+      return mClickPublisher;
+    }
+
+    @Override
+    public LayerOptionViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+      int layoutIdForListItem = R.layout.layer_list_item;
+      LayoutInflater inflater = LayoutInflater.from(mContex);
+      boolean shouldAttachToParentImmediately = false;
+      View view = inflater.inflate(layoutIdForListItem, parent, shouldAttachToParentImmediately);
+
+      return new LayerOptionViewHolder(view, mClickPublisher);
+    }
+
+    @Override
+    public void onBindViewHolder(LayerOptionViewHolder holder, int position) {
+      String key = wellnessItemIndex.get(position);
+      holder.bind(key, wellnessItems.get(key));
+    }
+
+    @Override
+    public int getItemCount() {
+      return wellnessItems.size();
+    }
+  }
+
+  private static class LayerOptionViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+    private TextView mOptionTitle;
+    private PublishSubject<String> mPublisher;
+
+    private String key;
+
+    public LayerOptionViewHolder(View itemView, PublishSubject<String> publisher) {
+      super(itemView);
+      itemView.setOnClickListener(this);
+      mPublisher = publisher;
+      mOptionTitle = (TextView) itemView.findViewById(R.id.tv_layer_item);
+    }
+
+    public void bind(String key, String value) {
+      this.key = key;
+      mOptionTitle.setText(value);
+    }
+
+    public String getKey() {
+      return key;
+    }
+
+    @Override
+    public void onClick(View view) {
+      mPublisher.onNext(key);
+    }
+  }
+}
