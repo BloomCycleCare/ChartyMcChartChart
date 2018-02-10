eaa2b34322509d5f876d64874e3e3306039ba1ee
parkeroth <parkeroth@gmail.com>
2018-02-10 17:10:58 +0100

    Incomplete create goal UI

--- app/src/main/java/com/roamingroths/cmcc/ui/profile/GoalDetailActivity.java
+++ app/src/main/java/com/roamingroths/cmcc/ui/profile/GoalDetailActivity.java
@@ -2,14 +2,22 @@ package com.roamingroths.cmcc.ui.profile;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 
 import com.roamingroths.cmcc.R;
 
 public class GoalDetailActivity extends AppCompatActivity {
 
+  private RecyclerView mGoalOptions;
+
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_goal_detail);
+
+    Toolbar toolbar = findViewById(R.id.toolbar);
+    setSupportActionBar(toolbar);
+    getSupportActionBar().setTitle("");
   }
 }
--- app/src/main/java/com/roamingroths/cmcc/ui/profile/ProfileActivity.java
+++ app/src/main/java/com/roamingroths/cmcc/ui/profile/ProfileActivity.java
@@ -1,5 +1,6 @@
 package com.roamingroths.cmcc.ui.profile;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
@@ -61,6 +62,7 @@ public class ProfileActivity extends AppCompatActivity {
     mFabGeneral.setOnClickListener(new View.OnClickListener() {
       @Override
       public void onClick(View v) {
+        startActivity(new Intent(ProfileActivity.this, GoalDetailActivity.class));
         mFam.collapse();
       }
     });
