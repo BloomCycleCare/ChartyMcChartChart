cd09d0ddeef2ecabaff7b2cf74eaff2f84817e44
parkeroth <parkeroth@gmail.com>
2020-09-04 16:35:01 +0200

    Create preference category for notifications

--- app_charting/src/main/java/com/bloomcyclecare/cmcc/notifications/ChartingReceiver.java
+++ app_charting/src/main/java/com/bloomcyclecare/cmcc/notifications/ChartingReceiver.java
@@ -3,7 +3,6 @@ package com.bloomcyclecare.cmcc.notifications;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.os.Build;
 
 import timber.log.Timber;
 
@@ -12,10 +11,6 @@ public class ChartingReceiver extends BroadcastReceiver {
   @Override
   public void onReceive(Context context, Intent intent) {
     Timber.i("Received intent to start charting reminder");
-    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-      context.startForegroundService(new Intent(context, ChartingService.class));
-    } else {
-      context.startService(new Intent(context, ChartingService.class));
-    }
+    context.startService(new Intent(context, ChartingService.class));
   }
 }
